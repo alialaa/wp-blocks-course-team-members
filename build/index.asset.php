ad7bf7d4871b518ee1690b1cffa632fb759b254e
Ali Alaa <hello@alialaa.com>
2021-11-14 11:33:06 +0200

    Added social links editor styling

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '79cb3d42cf59dfb9b3f2a3cb87a93648');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4d68c422dae7655569188533097be99c');
